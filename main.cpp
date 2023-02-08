e049d4c0afa653c3de81ae709fc1505ea38b5491
Sabelo Mk <90407948+LaplaceXero@users.noreply.github.com>
2023-02-08 10:25:36 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+int main() {
+	
+	int testCasea = 0;
+	int x; //starting time
+	int y; //ending time
+	
+	cin >>testCasea;
+	
+	for(int i = 0; i < testCasea; i++)
+	{
+	    cin >>x >>y;
+	    
+	    cout <<(y-x) <<endl;
+	}
+	return 0;
+}
